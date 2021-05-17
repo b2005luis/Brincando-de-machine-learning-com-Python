e3b90ff08c72a28a7f2369bc33f59beb714ea557
Luis Alberto Batista Pedroso <44144554+b2005luis@users.noreply.github.com>
2021-05-17 20:26:41 -0300

    Adjusts and updates in program and data

--- Classifier.py
+++ Classifier.py
@@ -6,21 +6,18 @@ data = pandas.read_csv("mega-sena.csv", header=0, sep=";")
 x_axis = data[["Coluna 1","Coluna 2","Coluna 3","Coluna 4","Coluna 5","Coluna 6"]]
 y_axis = data["Ganhadores Faixa 1"]
 
-classifier = MLPClassifier(hidden_layer_sizes=(30),
-                           early_stopping=False,
+classifier = MLPClassifier(hidden_layer_sizes=(60,120,120,60),
+                           early_stopping=True,
                            validation_fraction=0.75)
 
 k = y_axis.__len__() - 10
 lista = []
 for ix, row in x_axis[k:].iterrows():
     for item in row:
-        # if not lista.__contains__(item):
-            # lista.append(item)
-        lista.append(item)
+        if not lista.__contains__(item):
+            lista.append(item)
 
 classifier.fit(x_axis, y_axis)
-
-print("Esperados = [7, 31, 37, 42, 44, 56]")
 
 i = 1
 while i <= 7:
