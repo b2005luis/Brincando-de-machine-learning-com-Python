8c3947862b27386b50b6722db6b1ec73f46e25f0
Luis Alberto Batista <b2005.luis@gmail.com>
2021-09-04 14:06:25 -0300

    Update database and adjusts in network calibration

--- Mega-Sena-Regressor.py
+++ Mega-Sena-Regressor.py
@@ -14,14 +14,14 @@ data = DataFrame(download)
 x_axis = data[[0, 1, 2, 3, 4, 5]]
 y_axis = data[6]
 
-regressor = MLPRegressor(hidden_layer_sizes=(30,100,30),
+regressor = MLPRegressor(hidden_layer_sizes=(30, 30),
                          alpha=0.0,
                          beta_1=0.0,
                          beta_2=0.0,
                          learning_rate="adaptive")
 
-ks = y_axis.__len__() - 30
-ke = y_axis.__len__() - 15
+ks = y_axis.__len__() - 20
+ke = y_axis.__len__() - 2
 lista = []
 for ix, row in x_axis[ks:ke].iterrows():
     for item in row:
@@ -40,12 +40,11 @@ i = 1
 while i <= 7:
     to_predict = sample(lista, 6)
     predicted = regressor.predict([to_predict])
-    if predicted >= 1.0:
+    if predicted >= 0.0666666666666667 and predicted <= 0.1:
         for ix, row in esperados.iterrows():
             for e in row:
                 if to_predict.__contains__(e):
                     acertos.append(e)
-
         print("{} :: {}".format(sorted(to_predict, reverse=False), predicted))
         print("Acerto(s) :: {} = {} acertos".format(acertos, acertos.__len__()))
         i = i + 1
