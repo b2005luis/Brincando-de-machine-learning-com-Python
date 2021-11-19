7f39bb53020891733b2aa535701a171a237be9f8
Luis Alberto Batista Pedroso <44144554+b2005luis@users.noreply.github.com>
2021-11-19 08:37:51 -0300

    Chabges for accept multiple bets

--- Auto-Select-numbers-to-cart.js
+++ Auto-Select-numbers-to-cart.js
@@ -1,6 +1,19 @@
-var numeros = [1, 5, 6, 7, 8, 10, 11, 13, 15, 17, 18, 19, 20, 22, 23]
-numeros.map(function (n) {
-    n = (n < 10) ? ("#n0" + n) : ("#n" + n);
-    document.querySelector(n).click();
+var jogos = [
+    [5, 8, 13, 39, 45, 51],
+    [3, 15, 17, 20, 26, 31]
+];
+
+jogos.map(function (jogo) {
+    marcarJogo(jogo);
 });
-document.querySelector("#colocarnocarrinho").click();
+
+function marcarJogo(jogo) {
+
+    jogo.map(function (n) {
+        n = (n < 10) ? ("#n0" + n) : ("#n" + n);
+        document.querySelector(n).click();
+    });
+
+    document.querySelector("#colocarnocarrinho").click();
+
+}
